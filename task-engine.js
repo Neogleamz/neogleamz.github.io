951b8299998f6875b758ff492b455d204dccc857
neogleamz <services@neogleamz.com>
2026-05-16 00:49:41 -0500

    fix(task-engine): inject missing generateUUID utility function to unblock entity creation

--- task-engine.js
+++ task-engine.js
@@ -3,6 +3,13 @@
    A.I. Generated UI Takeover Module
    ========================================================================= */
 
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
 let isTaskPlannerOpen = false;
 
 let taskEngineDB = { taskz: [], cyclez: [], projectz: [], teams: [], comments: [], activity: [], tagz: [] };
